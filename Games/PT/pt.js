67adf2416599612da89ba160d4f2891a3a51ade8
Xanax <Merwwj@gmail.com>
2022-03-11 20:09:44 +0330

    completed PT JS

--- Games/PT/pt.js
+++ Games/PT/pt.js
@@ -49,17 +49,67 @@ const cardArr = [
     },
 ]
 
+let chosenCard = []
+let chosenCardId = []
+const score = []
+let scoreDisplay = document.querySelector('#score')
+
 cardArr.sort(()=> 0.5 - Math.random())
 const grid = document.querySelector('#grid')
 
 function createCard(){
-    for(let i = 0 ; i < 12 ; i++){
+    for(let i = 0 ; i < cardArr.length ; i++){
         const card = document.createElement('img')
         card.setAttribute('src' , 'images/blank.gif')
         card.setAttribute('data-id' , i)
-        console.log(card)
+        card.addEventListener('click' , flipCard )
         grid.append(card)
     }
 }
 
+function matchCheck(){
+    // should change to '#grid img' if more images were added
+    const cards = document.querySelectorAll('img'); 
+    const firstCardId = chosenCardId[0]
+    const secondCardId =  chosenCardId[1]
+
+    if(firstCardId === secondCardId){
+        alert(`You Clicked The Same Card`)
+        cards[firstCardId].setAttribute('src' , 'images/blank.gif')
+    }
+    else if(chosenCard[0] === chosenCard[1]){
+        alert(`You Found A Match`)
+        cards[firstCardId].setAttribute('src' , 'images/correct.png')
+        cards[secondCardId].setAttribute('src' , 'images/correct.png')
+        cards[firstCardId].removeEventListener('click',flipCard)
+        cards[secondCardId].removeEventListener('click',flipCard)
+        score.push(chosenCard)
+        scoreDisplay.textContent = score.length
+        
+    }else{
+        alert(`Not a Match, Try Again!`)
+        cards[firstCardId].setAttribute('src' , 'images/blank.gif')
+        cards[secondCardId].setAttribute('src' , 'images/blank.gif')
+    }
+    chosenCard = []
+    chosenCardId = []
+    if(score.length === cardArr.length/2){
+        alert(`Congrats, You have found them all`)
+    }
+}
+
+function flipCard(){
+    const cardId = this.getAttribute('data-id')
+    chosenCard.push(cardArr[cardId].name)
+    chosenCardId.push(cardId)
+
+    this.setAttribute('src' , cardArr[cardId].img)
+
+    if(chosenCard.length === 2){
+        setTimeout( matchCheck,1000)
+    }
+}
+
+
 createCard()
+
