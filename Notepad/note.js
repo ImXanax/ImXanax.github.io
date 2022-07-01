73fbfc2744c0914b9b38d05af4743582348a34fc
Xanax <Merwwj@gmail.com>
2022-07-01 12:08:42 +0430

    Note/added text input +responsive

--- Notepad/note.js
+++ Notepad/note.js
@@ -1,2 +1,8 @@
 const num = document.querySelectorAll('input[type="checkbox"]')
 console.log(num[0].checked)
+
+const btn = document.querySelector('.input-add')
+
+btn.addEventListener('click',()=>{
+    console.log(`clicked`)
+})
