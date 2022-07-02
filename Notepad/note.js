1a6814a8cf28aea3f0cf78ae2e98568b01e7e0f0
Xanax <Merwwj@gmail.com>
2022-07-02 13:00:52 +0430

    Note/initializing required components for card

--- Notepad/note.js
+++ Notepad/note.js
@@ -1,11 +1,25 @@
-const num = document.querySelectorAll('input[type="checkbox"]')
-const btn = document.querySelector('.input-add')
-const taskContainer = document.querySelector('.tasks')
-const cards = document.querySelectorAll('li[class="card"]')
+const num = document.querySelectorAll('input[type="checkbox"]');
+const btn = document.querySelector(".input-add");
+const taskContainer = document.querySelector(".tasks");
+const cards = document.querySelectorAll('li[class="card"]');
 // const cardPlaceholder = document.createElement('div')
 // cardPlaceholder.classList.add('next-card')
 // taskContainer.appendChild(cardPlaceholder)
 
+//creating card
+const createCard = () => {
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+  const newCardUtil = document.createElement("div");
+  newCardUtil.classList.add("card-util");
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  const task = document.createElement("p");
+  task.classList.add("item");
+
+};
 // adding tasks
 
 // checkmarking task + strikethrough effect
