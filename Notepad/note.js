1f7b5a52c4175cd3ae67b5bf0336291bca850315
Xanax <Merwwj@gmail.com>
2022-07-02 13:08:38 +0430

    Note/input createCard function

--- Notepad/note.js
+++ Notepad/note.js
@@ -7,19 +7,31 @@ const cards = document.querySelectorAll('li[class="card"]');
 // taskContainer.appendChild(cardPlaceholder)
 
 //creating card
-const createCard = () => {
-  const newCard = document.createElement("li");
-  newCard.classList.add("card");
-  const newCardUtil = document.createElement("div");
-  newCardUtil.classList.add("card-util");
-  const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete");
-  const task = document.createElement("p");
-  task.classList.add("item");
+function createCard() {
+  try {
+    const newCard = document.createElement("li");
+    newCard.classList.add("card");
+    const newCardUtil = document.createElement("div");
+    newCardUtil.classList.add("card-util");
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
+    const task = document.createElement("p");
+    task.classList.add("item");
 
-};
+    task.textContent = "testing functionality...";
+
+    newCard.appendChild(newCardUtil);
+    newCardUtil.appendChild(checkBox);
+    newCardUtil.appendChild(deleteBtn);
+    newCard.appendChild(task);
+
+    taskContainer.appendChild(newCard)
+  } catch (e) {
+    console.error("ERR:" + e);
+  }
+}
 // adding tasks
 
 // checkmarking task + strikethrough effect
