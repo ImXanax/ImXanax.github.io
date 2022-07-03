8af0217608c6dc740ffdef3582c328f04c26926f
Xanax <Merwwj@gmail.com>
2022-07-04 00:14:40 +0430

    Note/added placeholder for next card

--- Notepad/note.js
+++ Notepad/note.js
@@ -2,10 +2,16 @@ const num = document.querySelectorAll('input[type="checkbox"]');
 const btn = document.querySelector(".input-add");
 const taskContainer = document.querySelector(".tasks");
 const cards = document.querySelectorAll('li[class="card"]');
-const taskInput = document.querySelector('input[type="text"]')
-// const cardPlaceholder = document.createElement('div')
-// cardPlaceholder.classList.add('next-card')
-// taskContainer.appendChild(cardPlaceholder)
+const taskInput = document.querySelector('input[type="text"]');
+
+function addPlaceHolder() {
+  let placeHolder = document.querySelector('div[class="next-card"]');
+  placeHolder.remove();
+
+  placeHolder = document.createElement("div");
+  placeHolder.classList.add("next-card");
+  taskContainer.appendChild(placeHolder);
+}
 
 //creating card
 function createCard() {
@@ -21,19 +27,33 @@ function createCard() {
     const task = document.createElement("p");
     task.classList.add("item");
 
-    let todo = taskInput.value ? taskInput.value : 'Nothing Specified...';
+    let todo = taskInput.value ? taskInput.value : "Nothing Specified...";
 
-    task.textContent = todo
+    task.textContent = todo;
     newCard.appendChild(newCardUtil);
     newCardUtil.appendChild(checkBox);
     newCardUtil.appendChild(deleteBtn);
     newCard.appendChild(task);
 
-    taskContainer.appendChild(newCard)
+    taskContainer.appendChild(newCard);
+    //add initial placeholder
+    if (!document.querySelector('div[class="next-card"]')) {
+      const placeHolder = document.createElement("div");
+      placeHolder.classList.add("next-card");
+      taskContainer.appendChild(placeHolder);
+    } else {
+      addPlaceHolder();
+    }
   } catch (e) {
     console.error("ERR:" + e);
   }
 }
+
+taskInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    createCard();
+  } else return;
+});
 // adding tasks
 
 // checkmarking task + strikethrough effect
