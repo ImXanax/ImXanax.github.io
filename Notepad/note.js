574f2f3a6b27228a8a06350e29e86d4667ab868f
Xanax <Merwwj@gmail.com>
2022-07-04 00:18:24 +0430

    Note/commentate

--- Notepad/note.js
+++ Notepad/note.js
@@ -4,10 +4,13 @@ const taskContainer = document.querySelector(".tasks");
 const cards = document.querySelectorAll('li[class="card"]');
 const taskInput = document.querySelector('input[type="text"]');
 
+//adding placeholder for next card
 function addPlaceHolder() {
+  //prevent overlapping element
   let placeHolder = document.querySelector('div[class="next-card"]');
   placeHolder.remove();
 
+  //remake new element
   placeHolder = document.createElement("div");
   placeHolder.classList.add("next-card");
   taskContainer.appendChild(placeHolder);
@@ -15,49 +18,42 @@ function addPlaceHolder() {
 
 //creating card
 function createCard() {
-  try {
-    const newCard = document.createElement("li");
-    newCard.classList.add("card");
-    const newCardUtil = document.createElement("div");
-    newCardUtil.classList.add("card-util");
-    const checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete");
-    const task = document.createElement("p");
-    task.classList.add("item");
+  //fetching required elements
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+  const newCardUtil = document.createElement("div");
+  newCardUtil.classList.add("card-util");
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  const task = document.createElement("p");
+  task.classList.add("item");
 
-    let todo = taskInput.value ? taskInput.value : "Nothing Specified...";
+  //deals with empty inputs
+  let todo = taskInput.value ? taskInput.value : "Nothing Specified...";
 
-    task.textContent = todo;
-    newCard.appendChild(newCardUtil);
-    newCardUtil.appendChild(checkBox);
-    newCardUtil.appendChild(deleteBtn);
-    newCard.appendChild(task);
+  //creates a new card
+  task.textContent = todo;
+  newCard.appendChild(newCardUtil);
+  newCardUtil.appendChild(checkBox);
+  newCardUtil.appendChild(deleteBtn);
+  newCard.appendChild(task);
+  taskContainer.appendChild(newCard);
 
-    taskContainer.appendChild(newCard);
-    //add initial placeholder
-    if (!document.querySelector('div[class="next-card"]')) {
-      const placeHolder = document.createElement("div");
-      placeHolder.classList.add("next-card");
-      taskContainer.appendChild(placeHolder);
-    } else {
-      addPlaceHolder();
-    }
-  } catch (e) {
-    console.error("ERR:" + e);
+  //add initial placeholder if it doesnt exist already
+  if (!document.querySelector('div[class="next-card"]')) {
+    const placeHolder = document.createElement("div");
+    placeHolder.classList.add("next-card");
+    taskContainer.appendChild(placeHolder);
+  } else {
+    addPlaceHolder();
   }
 }
 
+//Accept 'Enter' as alternative to add tasks
 taskInput.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     createCard();
   } else return;
 });
-// adding tasks
-
-// checkmarking task + strikethrough effect
-
-// removing tasks
-
-// appending card placeholder at the end
